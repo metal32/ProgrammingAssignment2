3d9220fdcfdef15ed84e6d05f0e8a08cb882fabd
metal32 <ayus097836@gmail.com>
2016-07-19 04:35:33 +0530

    Intial funtion input for cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## X is our matrix in function makeacheMatrix
+## The makecacheMatrix function will return a list of functions to:
+## a)set the matrix
+## b)get the matrix
+## c)set the inverse
+## d)get the inverse
+## The function is made to act as an input for CacheSolve function 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+inv<-NULL
+set<-function(y){
+## <<- It is used to assign a value to an object of another environment 
+    x<<-y
+  inv<<-NULL
+  }
+get<-function()x
+setinv<-function(inverse) inv<<-inverse
+getinv<-function() inv
+list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
-
 
 ## Write a short comment describing this function
 
